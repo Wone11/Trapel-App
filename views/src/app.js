b342d9fa571139985db8e85053bcd767958f4923
Wone11 <gelayegeremew@gmail.com>
2023-04-27 00:44:58 +0300

    coponents added

--- views/src/app.js
+++ views/src/app.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from 'react';
+import {CssBaseline, Grid} from '@material-ui/core'
 
-export default function app() {
+import Header    from './Components/Header/Header';
+import Maps      from './Components/Maps/Maps';
+import Details   from './Components/Details/Details';
+import List      from './Components/List/List';
+
+
+
+const app = () => {
   return (
-    <div>app</div>
-  )
+    <>
+          <CssBaseline />
+          <Header />
+          <Grid container spacing={3} style={{'width':'100%'}}>
+           <Grid item xs={12} md ={2}>
+                  <List />
+           </Grid>
+           <Grid item xs={12} md ={8}>
+                  <Maps></Maps>
+           </Grid>
+           <Grid item xs={12} md ={2}>
+                  <Details />
+           </Grid>
+          </Grid>
+    </>
+  );
 }
+
+export default app;
