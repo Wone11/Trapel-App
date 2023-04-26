d86fd3a50b9ca4ce461a5caa60954400a325bf27
Wone11 <gelayegeremew@gmail.com>
2023-04-27 01:41:49 +0300

    styles added

--- views/src/Components/Header/Header.jsx
+++ views/src/Components/Header/Header.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import {Autocomplete, autocomplete} from '@react-google-maps/api'
+
+import { AppBar,Toolbar, Typography,InputBase,Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search'
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header!</h1>
-    </div>
+    <>
+          <AppBar position="static">
+              <Toolbar className={classes.toolbar}>
+                  <Typography variant="h5" className={classes.title}>
+                      Drug Store Finder
+                  </Typography>
+                  <Box display="flex">
+                      <Typography variant="h6" className={classes.details}>
+                         Explore New Place
+                      </Typography>
+                      <Autocomplete>
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon/>
+                            </div>
+                            <InputBase placeholder="Search..." className={{root:classes.inputRoot,input:classes.classes.inputInput}}/>
+                        </div>
+                      </Autocomplete>
+                  </Box>
+              </Toolbar>
+          </AppBar>
+    </>
   );
 }
 
